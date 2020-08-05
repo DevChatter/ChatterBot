e90e08eda1857390b604bfcdf8f73602743c16be
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-08-04 22:42:31 -0400

    Adding a class to request a twitch oauth token

---
+++ ChatterBot.Infra.Twitch/TwitchAuthentication.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChatterBot.Infra.Twitch
+{
+    public class TwitchAuthentication
+    {
+        public const string ClientId = ""; // TODO: Pull from Config
+
+        public List<string> States { get; set; } = new List<string>();
+
+        public const string RedirectUrl = "http://localhost:5834"; // TODO: Pull from Config
+
+        public static string[] Scopes =
+        {
+            "bits:read",
+            "channel:edit:commercial",
+            "channel:read:hype_train",
+            "channel:read:subscriptions",
+            "channel:moderate",
+            "chat:edit",
+            "chat:read",
+            "whispers:read",
+            "whispers:edit",
+        };
+        public static string ScopeString = string.Join("+", value: Scopes);
+
+
+        public string GetUrl()
+        {
+            string state = Guid.NewGuid().ToString();
+            States.Add(state);
+
+            return $"https://id.twitch.tv/oauth2/authorize?client_id={ClientId}&redirect_uri={RedirectUrl}&response_type=token&scope={ScopeString}&state={state}";
+        }
+    }
+}
