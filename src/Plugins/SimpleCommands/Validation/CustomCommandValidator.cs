053d14d23b7210ddffffa6e72dc8f8a0e7b44fff
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-08-11 23:39:02 -0400

    Add validation rules

--- src/Plugins/SimpleCommands/Validation/CustomCommandValidator.cs
+++ src/Plugins/SimpleCommands/Validation/CustomCommandValidator.cs
@@ -1,17 +1,23 @@
 ﻿using ChatterBot.Domain.Validation;
 using FluentValidation;
+using System.Diagnostics.CodeAnalysis;
 
 namespace ChatterBot.Plugins.SimpleCommands.Validation
 {
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "CA1812", Justification = "Compiler dosen't understand dependency injection")]
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "Fluent Validators are never used as collections directly")]
+    [SuppressMessage("Build", "CA1812", Justification = "Compiler doesn't understand dependency injection")]
+    [SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "Fluent Validators are never used as collections directly")]
     internal class CustomCommandValidator : AbstractValidator<CustomCommand>, ICustomCommandValidator
     {
         public CustomCommandValidator()
         {
-            // TOOD: Add additional validation rules.
-            this.RuleFor(x => x.CommandWord).NotEmpty().WithMessage("{PropertyName} must not be empty.");
-            this.RuleFor(x => x.CommandWord).Must(x => x.StartsWith("!")).WithMessage("{PropertyName} must start with a '!' character.");
+            RuleFor(x => x.CommandWord).NotEmpty().WithMessage("{PropertyName} must not be empty.");
+            RuleFor(x => x.Response).NotEmpty().WithMessage("{PropertyName} must not be empty.");
+
+            RuleFor(x => x.CooldownTime).GreaterThanOrEqualTo(0).WithMessage("{PropertyName} cannot be negative.");
+            RuleFor(x => x.CooldownTime).LessThanOrEqualTo(1440).WithMessage("{PropertyName} cannot be more than a day.");
+
+            RuleFor(x => x.UserCooldownTime).GreaterThanOrEqualTo(0).WithMessage("{PropertyName} cannot be negative.");
+            RuleFor(x => x.UserCooldownTime).LessThanOrEqualTo(1440).WithMessage("{PropertyName} cannot be more than a day.");
         }
     }
 }
