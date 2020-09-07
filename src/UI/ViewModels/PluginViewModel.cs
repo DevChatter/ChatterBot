892febe8d083d405975cf0404b8b5b5fc0de0dec
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2020-09-07 15:31:49 -0400

    WIP - Save changes to PluginInfo

--- src/UI/ViewModels/PluginViewModel.cs
+++ src/UI/ViewModels/PluginViewModel.cs
@@ -1,8 +1,9 @@
-﻿using ChatterBot.Domain.Plugins;
+﻿using ChatterBot.Data;
+using ChatterBot.Domain.Plugins;
 using ChatterBot.Domain.State;
 using ChatterBot.UI.Views;
 using MahApps.Metro.IconPacks;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.ComponentModel;
 
 namespace ChatterBot.UI.ViewModels
@@ -10,14 +11,26 @@ namespace ChatterBot.UI.ViewModels
     public class PluginViewModel : MenuItemViewModel
     {
         private readonly IPluginSet _pluginSet;
+        private readonly IDataStore _dataStore;
 
         public BindingList<PluginInfo> Plugins => _pluginSet.Plugins;
 
-        public PluginViewModel(IPluginSet pluginSet)
+        public PluginViewModel(IPluginSet pluginSet, IDataStore dataStore)
             : base(new PackIconMaterial { Kind = PackIconMaterialKind.Puzzle }, "Plugins",
                 "Custom Plugins", new PluginView())
         {
             _pluginSet = pluginSet;
+            _dataStore = dataStore;
+
+            _pluginSet.Plugins.ListChanged += PluginsOnListChanged;
+        }
+
+        private void PluginsOnListChanged(object sender, ListChangedEventArgs e)
+        {
+            _dataStore.SaveEntities(_pluginSet.Plugins);
+
+            // REMOVE THIS!!!
+            List<PluginInfo> pluginInfos = _dataStore.GetEntities<PluginInfo>();
         }
     }
 }
